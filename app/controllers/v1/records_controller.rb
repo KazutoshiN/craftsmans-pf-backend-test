6dc0107d285a8222b35f18b58b132206f6a2885f
Akira Saito <a-saito@bpm-gr.co.jp>
2019-03-14 09:07:07 +0900

    rubocopによるスタイル修正

--- app/controllers/v1/records_controller.rb
+++ app/controllers/v1/records_controller.rb
@@ -1,44 +1,46 @@
-class V1::RecordsController < ApplicationController
-  before_action :set_record, only: [:show, :update, :destroy]
+module V1
+  class RecordsController < ApplicationController
+    before_action :set_record, only: %i[show update destroy]
 
-  # GET /records
-  def index
-    @records = Record.all
+    # GET /records
+    def index
+      @records = Record.all
 
-    render json: @records
-  end
-
-  # GET /records/1
-  def show
-    render json: @record
-  end
-
-  # POST /records
-  def create
-    @record = Record.new(record_params)
-
-    if @record.save
-      render json: @record, status: :created, location: @record
-    else
-      render json: @record.errors, status: :unprocessable_entity
+      render json: @records
     end
-  end
 
-  # PATCH/PUT /records/1
-  def update
-    if @record.update(record_params)
+    # GET /records/1
+    def show
       render json: @record
-    else
-      render json: @record.errors, status: :unprocessable_entity
     end
-  end
 
-  # DELETE /records/1
-  def destroy
-    @record.destroy
-  end
+    # POST /records
+    def create
+      @record = Record.new(record_params)
 
-  private
+      if @record.save
+        render json: @record, status: :created, location: @record
+      else
+        render json: @record.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /records/1
+    def update
+      if @record.update(record_params)
+        render json: @record
+      else
+        render json: @record.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /records/1
+    def destroy
+      @record.destroy
+    end
+
+    private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_record
       @record = Record.find(params[:id])
@@ -48,4 +50,5 @@ class V1::RecordsController < ApplicationController
     def record_params
       params.fetch(:record, {})
     end
+end
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Unicorn設定ファイル
 # 次のURLのサンプルをベースにしている。
 # http://unicorn.bogomips.org/examples/unicorn.conf.rb
@@ -7,16 +6,16 @@
 worker_processes 2
 
 # リクエスト待ち受け口、TCPとUNIXドメインとが指定可能。
-#listen "/var/lib/redmine/tmp/sockets/redmine.sock", :backlog => 32
-listen "/var/lib/analysis/unicorn.sock", :backlog => 32
+# listen "/var/lib/redmine/tmp/sockets/redmine.sock", :backlog => 32
+listen '/var/lib/analysis/unicorn.sock', backlog: 32
 # listen 8282, :tcp_nopush => true
 
 # タイムアウト秒数
 timeout 30
 
 # 稼働中のプロセスのPIDを書いておくファイル。
-#pid "/var/lib/redmine/tmp/pids/redmine.pid"
-pid "/home/vagrant/analysis/tmp/pids/unicorn.pid"
+# pid "/var/lib/redmine/tmp/pids/redmine.pid"
+pid '/home/vagrant/analysis/tmp/pids/unicorn.pid'
 
 # デーモンで起動すると標準出力／標準エラー出力が/dev/nullになるので、
 # それぞれログファイルに出力する。
@@ -36,35 +35,32 @@ preload_app true
 # アプリケーションを呼ぶ前にチェックする。
 check_client_connection false
 
-before_fork do |server, worker|
+before_fork do |_server, _worker|
   # Railsでpreload_appをtrueにしているときは強く推奨
-  defined?(ActiveRecord::Base) and
+  defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do |server, worker|
+after_fork do |_server, worker|
   # Railsでpreload_appをtrueにしているときは必須
-  defined?(ActiveRecord::Base) and
+  defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.establish_connection
 
   begin
-    uid, gid = Process.euid, Process.egid
+    uid = Process.euid
+    gid = Process.egid
     # user, group = "redmine", "redmine"
     # ENV["HOME"] = "/home/redmine"
     target_uid = Etc.getpwnam(user).uid
     target_gid = Etc.getgrnam(group).gid
     worker.tmp.chown(target_uid, target_gid)
-    if uid != target_uid or gid != target.gid
+    if (uid != target_uid) || (gid != target.gid)
       Process.initgroups(user, target_gid)
       Process::GID.change_privilege(target_gid)
       Process::UID.change_privilege(target_uid)
     end
-  rescue
-    if RAILS_ENV = "development"
-      STDERR.puts "could not change user, oh well"
-    else
-      STDERR.puts "could not change user, oh well"
-      raise e
-    end
+  rescue StandardError
+    STDERR.puts 'could not change user, oh well'
+    raise 0 if RAILS_ENV != 'development'
   end
 end
